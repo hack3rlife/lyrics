fdc359d35c142ed99d64eb4c42196a5c9f7b0035
hack3rlife <hack3rlife@outlook.com>
2016-03-29 19:40:47 -0700

    AlbumController & Views
    Adding default scaffolding AlbumController and its related views

---
+++ mvc/MvcLyrics/MvcLyrics/Controllers/AlbumsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcLyrics.Models;
+
+namespace MvcLyrics.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private LyricsDbContext db = new LyricsDbContext();
+
+        // GET: Albums
+        public async Task<ActionResult> Index()
+        {
+            var albums = db.Albums.Include(a => a.Artist).Include(a => a.Song);
+            return View(await albums.ToListAsync());
+        }
+
+        // GET: Albums/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "FirstName");
+            ViewBag.SongId = new SelectList(db.Songs, "SongId", "SongName");
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ArtistId,SongId,AlbumId,AlbumName,TrackNum,Length,CreatedDate,CreatedBy,LastUpdate,UpdatedBy")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "FirstName", album.ArtistId);
+            ViewBag.SongId = new SelectList(db.Songs, "SongId", "SongName", album.SongId);
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "FirstName", album.ArtistId);
+            ViewBag.SongId = new SelectList(db.Songs, "SongId", "SongName", album.SongId);
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ArtistId,SongId,AlbumId,AlbumName,TrackNum,Length,CreatedDate,CreatedBy,LastUpdate,UpdatedBy")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "FirstName", album.ArtistId);
+            ViewBag.SongId = new SelectList(db.Songs, "SongId", "SongName", album.SongId);
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Album album = await db.Albums.FindAsync(id);
+            db.Albums.Remove(album);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
